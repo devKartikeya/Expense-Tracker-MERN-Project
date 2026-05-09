8ae4b6340d3df6b81ccf858d2ba2284d656f31d7
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-05-09 13:54:54 +0530

    Fix mail url

--- Backend/routes/mail.route.js
+++ Backend/routes/mail.route.js
@@ -16,7 +16,7 @@ router.post("/forgot-password", async (req, res) => {
   user.resetPasswordExpires = Date.now() + 15 * 60 * 1000;
   await user.save();
 
-  const resetLink = `expense-tracker-mern-project-seven.vercel.app/reset-password/${token}`;
+  const resetLink = `https://expense-tracker-mern-project-seven.vercel.app/reset-password/${token}`;
   await sendResetEmail(email, resetLink);
 
   res.json({ message: "Password reset link sent to your email." });
