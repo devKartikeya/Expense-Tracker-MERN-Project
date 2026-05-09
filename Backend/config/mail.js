655e8d385477bdc9aadcfbf93e5f747c46dc398c
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-05-09 13:47:02 +0530

    Update gmail host

--- Backend/config/mail.js
+++ Backend/config/mail.js
@@ -2,7 +2,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
