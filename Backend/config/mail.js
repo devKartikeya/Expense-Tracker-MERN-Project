5a9420f840e6a79c220047df2507b464957dc20d
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-05-10 00:00:52 +0530

    Add connectionTimeout

--- Backend/config/mail.js
+++ Backend/config/mail.js
@@ -9,6 +9,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+
+  connectionTimeout: 20000
 });
 
 const sendResetEmail = async (to, resetLink) => {
