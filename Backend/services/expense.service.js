0c466c006309c0897315e384799434ec2990d773
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-05-29 14:20:09 +0530

    Add Custom addition of categories into the DB Feature

--- Backend/services/expense.service.js
+++ Backend/services/expense.service.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Expense = require('../models/expenses.model');
+const Catgeory = require("../models/category.model")
 
 async function expense(userId, amount, category, description, date) {
     const newExpense = await Expense.create({
@@ -9,6 +10,11 @@ async function expense(userId, amount, category, description, date) {
         description,
         date: date ? date : undefined
     });
+    // Check if category exists, if not create it
+    const existingCategory = await Catgeory.findOne({ name: category });
+    if (!existingCategory) {
+        await Catgeory.create({ name: category });
+    }
     return newExpense;
 }
 
