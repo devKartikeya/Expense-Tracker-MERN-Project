4fe13791d089f87243be05218306dbf0ff49daff
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-06-03 20:36:11 +0530

    fix: fix layouts for mobile screens

--- Frontend/src/components/DashboardButton.jsx
+++ Frontend/src/components/DashboardButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({ onClick, command, bgColor }) => {
+const DashboardButton = ({ onClick, command, bgColor }) => {
     return (
         <button
-            className="h-12 md:w-[130px] w-[130px] bg-gradient-to-r from-pink-400 to-pink-600 rounded-xl text-white font-semibold shadow-lg hover:shadow-xl cursor-pointer transition-transform duration-300 ease-in-out md:my-5 my-3 hover:bg-gradient-to-r hover:from-blue-600 hover:to-cyan-600"
+            className="h-12 md:w-[130px] w-[170px] bg-gradient-to-r from-pink-400 to-pink-600 rounded-xl text-white font-semibold shadow-lg hover:shadow-xl cursor-pointer transition-transform duration-300 ease-in-out md:my-5 my-3 hover:bg-gradient-to-r hover:from-blue-600 hover:to-cyan-600"
             onClick={onClick}
         >
             {command}
@@ -11,4 +11,4 @@ const Button = ({ onClick, command, bgColor }) => {
     )
 }
 
-export default Button
+export default DashboardButton
